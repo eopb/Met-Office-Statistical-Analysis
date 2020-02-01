a9482a316be3c39f3fb463d2b01f3084caf580f2
Ethan Brierley <ethanboxx@gmail.com>
2020-02-01 16:42:34 +0000

    remove unneeded function

--- daily-maximum-gust-from-the-south-at-heathrow-may-to-october.r
+++ daily-maximum-gust-from-the-south-at-heathrow-may-to-october.r
@@ -1,7 +1,4 @@
 table <- read.table("data/cant-remember-where-this-is-from.csv", header=TRUE, sep=",", fileEncoding = "UTF-8")
-south <- function(x) {
-    x == "S"
-}
 
 table <- subset(table, grepl("S", Cardinal.Direction, fixed=TRUE))
 
